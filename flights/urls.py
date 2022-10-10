a1bf470619c318517c8a957960a8842532044a58
iNightjar <mohamedmedhateltlawy@gmail.com>
2022-10-10 22:44:34 +0200

    route to flight app with it's id

--- flights/urls.py
+++ flights/urls.py
@@ -1,8 +1,11 @@
 from operator import index
 from unicodedata import name
 from django.urls import path
+
+from flights.models import Flight
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<int:flight_id>", views.flight,name="flight")
 ]
