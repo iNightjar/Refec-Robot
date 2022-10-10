45109bb7998960b306c0623c1df094ef7ae3bdcb
iNightjar <mohamedmedhateltlawy@gmail.com>
2022-10-10 22:40:50 +0200

    flight function modified

--- flights/views.py
+++ flights/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
 
-import flights
+from django.shortcuts import render
 
 from .models import Flight
 # Create your views here.
@@ -8,4 +7,12 @@ from .models import Flight
 def index(request):
     return render(request, "flights/index.html", {
         "flights" : Flight.objects.all()
+    })
+
+
+def flight(request, flight_id):
+    flight = Flight.objects.get(pk=flight_id)
+    return render(request, "flights/flight.html", {
+        "flight": flight,
+        "passengers": flight.passengers.all()
     })
