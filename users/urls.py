ce2aeb7b96bc80274a80daa49ab6fbb7735c6da6
iNightjar <mohamedmedhateltlawy@gmail.com>
2022-10-12 03:56:39 +0200

    logout page route
    Co-authored-by: CodeCracer <sicarioeg4@gmail.com>

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,9 @@
-from operator import index
-from unicodedata import name
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
-    path("logout", views.logout_view, name="logout")
+    path("logout", views.logout_view, name="logout"),
 ]
 
